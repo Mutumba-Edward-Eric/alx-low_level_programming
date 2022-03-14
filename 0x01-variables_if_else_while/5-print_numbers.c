128516438896bd74be6eafbed0efd39e2f977c93
Mutumba-Edward-Eric <edwardstyles@gmail.com>
2022-03-13 20:13:28 -0700

    Variables if else while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,10 +5,12 @@
  * main - main block
  * Description: prints all single digit numbers of base 10
  * starting from 0
+ * Return: 0
  */
 int main(void)
 {
 	char c = '0';
+
 	while (c <= '9')
 	{
 		putchar(c);
