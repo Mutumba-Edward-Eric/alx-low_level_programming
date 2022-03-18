6e9e01404818ad7013cc011e4c1532105bb3ace6
Mutumba-Edward-Eric <edwardstyles@gmail.com>
2022-03-18 13:00:09 -0700

    Commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,9 +9,16 @@ void print_line(int n)
 {
 	int 1;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		_putchar(95);
+		for (i = 0; i < n; i++)
+	{
+		_putchar('_');
 	}
 	_putchar('\n');
+}
+else
+{
+	_putchar('\n');
+}
 }
