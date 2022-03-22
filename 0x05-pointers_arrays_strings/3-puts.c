c769c5db8230bda36b16dbf71e9b396d461936e7
Mutumba-Edward-Eric <edwardstyles@gmail.com>
2022-03-22 12:53:24 -0700

    Puts a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,20 +3,18 @@ include "main.h"
 /**
  * _puts - prints a string, followed by a new line.
  * @str: input string.
- * Return: no return.
+ * Return: No Return
  */
 void _puts(char *str)
 {
-	int count = 0;
+	int i;
 
-	while (count >= 0)
+	i = 0
+	while (*str != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		_putchar(*str);
+		i++;
+		str++;
 	}
+	_putchar('\n')
 }
