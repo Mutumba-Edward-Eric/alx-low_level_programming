7b93ca9cb3ed6d0ff5e7bcf1b546ea41ce819c72
Mutumba-Edward-Eric <edwardstyles@gmail.com>
2022-03-24 00:16:35 -0700

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,20 @@
 #include "main.h"
 
 /**
- * _strcat - concentrates two strings.
+ * _strncpy - copies a string
  * @dest: destination.
- * @src: source.
- * Return: the pointer ot dest.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
-char *_strcat(char *dest, char *src)
+
+char *_strncpy(char *dest, char *src, int n)
 {
-	int count = 0, count1 = 0;
+	int i;
 
-	while(*(dest + count) != '\0')
-	{
-		count++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-	while (count1 >= 0)
-	{
-		*(dest + count) = *(src + count1);
-		if (*(src + count1) == '\0')
-			break;
-		count++;
-		count1++;
-	}
 	return (dest);
 }
