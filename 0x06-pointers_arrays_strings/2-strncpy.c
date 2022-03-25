8718a8f697efffe2e9b7e48204becc7f9408a780
Mutumba-Edward-Eric <edwardstyles@gmail.com>
2022-03-25 13:04:59 -0700

    Commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,20 @@
-include "main.h"
-
+#include "main.h"
 /**
  * _strncpy - copies a string
- * @author: Muts Edward
  * @dest: destination.
  * @src: source.
  * @n: amount of bytes from src.
  * Return: the pointer to dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
